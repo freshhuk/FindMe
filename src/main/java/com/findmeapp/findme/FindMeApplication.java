01ec96f56c8470f6624ae22352a626141f47ba38
freshhuk <freshhuk13@gmail.com>
2024-07-10 11:09:42 +0300

    Init

---
+++ src/main/java/com/findmeapp/findme/FindMeApplication.java
@@ -0,0 +1,13 @@
+package com.findmeapp.findme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/findmeapp/findme/FindMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findmeapp.findme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
