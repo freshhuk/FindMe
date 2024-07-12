f7177bdae34fc255b311565761d1daadc42f78f3
freshhuk <freshhuk13@gmail.com>
2024-07-12 23:43:04 +0300

    Created end point

--- src/main/java/com/findmeapp/findme/Controllers/APIController.java
+++ src/main/java/com/findmeapp/findme/Controllers/APIController.java
@@ -1,14 +1,28 @@
 package com.findmeapp.findme.Controllers;
 
+import com.findmeapp.findme.Services.FindLogic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 
 @RestController
 public class APIController {
 
-    @GetMapping("/getSiluet")
-    public int getSiluet(){
+    private final FindLogic findSilhouetteService;
 
-        return 0;
+    @Autowired
+    public APIController(FindLogic findSilhouetteService){
+        this.findSilhouetteService = findSilhouetteService;
     }
+
+    @GetMapping("/getSilhouette")
+    public ResponseEntity<String> getSilhouette(MultipartFile image){
+
+        int count = findSilhouetteService.getSilhouette(image);
+        return ResponseEntity.ok().body("Cont - " + count);
+    }
+
 }
--- src/main/java/com/findmeapp/findme/Services/FindLogic.java
+++ src/main/java/com/findmeapp/findme/Services/FindLogic.java
@@ -1,16 +1,41 @@
 package com.findmeapp.findme.Services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.awt.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+
+import java.io.IOException;
 
 
 @Service
 public class FindLogic {
 
-    public int getSiluet(Image image){
+    public int getSilhouette(MultipartFile image){
+        try {
+            // Read image
+            BufferedImage bufferedImage = ImageIO.read(image.getInputStream());
 
-        int num = image.getHeight(image);
+            //Logic save in db or check it
+
+
+            //Get count of silhouette
+            int countSilhouette = findSilhouette(bufferedImage);
+
+            return countSilhouette;
+
+        } catch (IOException ex) {
+            System.out.println("Ops... " + ex);
+            return 0;
+        }
+    }
+
+    private int findSilhouette(BufferedImage image){
         return 0;
     }
+    private void getUploadedImage(){
+
+    }
+
 }
