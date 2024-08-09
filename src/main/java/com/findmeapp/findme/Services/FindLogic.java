2f9db836de82063f6127ae6301b7177f40c421d2
freshhuk <freshhuk13@gmail.com>
2024-08-09 23:29:40 +0300

    Working

--- src/main/java/com/findmeapp/findme/Services/FindLogic.java
+++ src/main/java/com/findmeapp/findme/Services/FindLogic.java
@@ -10,7 +10,6 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -65,6 +64,15 @@ public class FindLogic {
         //Как вариант решение проходить по массиву пиксилей сверху в низ а потом слева в право, и просматривать
         //Обьекты которые заканчиваются по границе и заканчиватся
 
+        for (int y = 0; y < image.getHeight(); y++){
+            for(int x = 0; x < image.getWidth(); x++){
+
+                Color color = new Color(image.getRGB(x, y));
+                if(color != bgColor){
+
+                }
+            }
+        }
 
         return 13;
     }
